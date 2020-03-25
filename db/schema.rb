77262d8187e8c68da6e1de8106a08c018a6c078e
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2020-03-25 13:43:50 +0000

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324125127) do
+ActiveRecord::Schema.define(version: 20200325131827) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20200324125127) do
     t.string "reset_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
