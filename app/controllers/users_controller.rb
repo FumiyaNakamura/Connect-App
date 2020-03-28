18541cca33294fc16d860e09bf55e4b8a7348ae2
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2020-03-28 07:38:21 +0000

    Repair Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      login @user
+      log_in @user
       flash[:success] = "ログインしました"
       redirect_to @user
     else
