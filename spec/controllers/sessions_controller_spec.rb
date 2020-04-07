290af9f5756c67213442f7deb7f8de6e7eb8c0d7
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2020-04-07 13:30:26 +0000

    RSpec アカウント有効化 パスワード再設定

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,8 +7,13 @@ RSpec.describe SessionsController, type: :controller do
     end
     
     describe "#create" do
+        it "ログインできれば、ユーザー詳細画面に遷移する" do
+            post :create, params: { session: { email: "tatsuro6@example.com", password: "password", remember_me: 1 } }
+            expect(response).to redirect_to @user
+        end
+        
         it "パスワードが間違っている時、ログイン画面にレンダリングする" do
-            post :create, params: { session: { email: "tatsuro1@example.com", password: "aaaaaa",remember_me: 1 } }
+            post :create, params: { session: { email: "tatsuro6@example.com", password: "aaaaaa", remember_me: 1 } }
             expect(response).to render_template "sessions/new"
         end
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,22 @@ FactoryBot.define do
     name {"tatsuro"}
     sequence(:email) { |n| "tatsuro#{n}@example.com"}
     password {"password"}
+    activated { true }
+  end
+  
+  factory :unactivate_user, class: User do
+    name { "yoshi" }
+    email { "yoshi@example.com" }
+    password {"password"}
+  end
+  
+  factory :activate_user, class: User do
+    name { "yoshi" }
+    email { "yoshi@example.com" }
+    password {"password"}
+    activated {true}
+    reset_token {"k1CisE9s62fDZl_VWv4ADw"}
+    reset_digest {"$2a$04$T2Mt/LmUaTV9MhhBrhL5b.G.dNhN9GNNGaGIobsYKgUm5dsnzso5q"}
+    reset_sent_at {Time.zone.now}
   end
 end
