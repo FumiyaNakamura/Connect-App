c86d7b485610461993de827272fe8d4448d11bd5
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2020-04-09 12:33:20 +0000

    コメント削除機能　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find_by(id: params[:post_id])
+    comment = Comment.find_by(post_id: params[:post_id], user_id: current_user.id)
+    binding.pry
     comment.destroy
     flash[:success] = "コメントを削除しました"
     redirect_to request.referer || root_url
