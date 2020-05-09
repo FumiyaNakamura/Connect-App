e5b1390ada92e83ce9856ae2d9e8ac45ba649d10
Fumiya Nakamura <fumiya.613.hand.v22@icloud.com>
2020-05-09 11:24:28 +0000

    ゲストユーザーログイン時のflashメッセージを変更

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -3,8 +3,8 @@ class GuestSessionsController < ApplicationController
   def create
     @user = User.find_by(email: 'test@example.com')
     log_in(@user)
-    flash[:success] = 'ゲストユーザーでログインしました'
-    flash[:warning] = 'よろしくお願いします！'
+    flash[:success] = 'ゲストユーザーでログインしました。'
+    flash[:warning] = 'ゲストユーザーは、ユーザー編集機能以外を使用できます。よろしくお願いします！'
     redirect_to @user
   end
 end
